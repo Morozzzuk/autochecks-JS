f89b5f125006eaacf51ed62c758ad59f396a6a7b
Morozzzuk <115870040+Morozzzuk@users.noreply.github.com>
2023-02-17 00:21:07 +0200

    update to 16/20

--- module 5.js
+++ module 5.js
@@ -81,5 +81,282 @@
 
 //   module 5 4/20
 
+// const parent = {
+//   name: "Stacey",
+//   surname: "Moore",
+//   age: 54,
+//   heritage: "Irish",
+// };
+// // Change code below this line
 
+// const child = Object.create(parent);
+
+// // Change code above this line
+// child.name = "Jason";
+// child.age = 27;
+
+//   module 5 5/20
+
+// const ancestor = {
+//   name: "Paul",
+//   age: 83,
+//   surname: "Dawson",
+//   heritage: "Irish",
+// };
+// // Change code below this line
+
+// const parent = Object.create(ancestor);
+// parent.name = "Stacey";
+// parent.surname = "Moore";
+// parent.age = 54;
+
+// const child = Object.create(parent);
+// child.name = "Jason";
+// child.age = 27;
+
+// // Change code above this line
+
+//   module 5 6/20
+// class Car {
   
+// }
+
+//   module 5 7/20
+
+// class Car {
+//   // Change code below this line
+// constructor(brand, model, price) {
+//   this.brand = brand;
+//   this.model = model;
+//   this.price = price;
+// }
+
+//   module 5 8/20
+
+// class Car {
+//   // Change code below this line
+//   constructor({brand, model, price}) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+//   // Change code above this line
+// }
+
+//   module 5 9/20
+
+// class Car {
+//   constructor({ brand, model, price }) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+//   // Change code below this line
+// getPrice() {
+//   return this.price;
+// }
+// changePrice(newPrice) {
+//   return this.price = newPrice;
+// }
+
+//   // Change code above this line
+// }
+
+//   module 5 10/20
+
+// class Storage {
+//   constructor(items){
+//     this.items = items;
+//   }
+//   getItems() {
+//     return this.items;
+//   }
+//   addItem(newItem) {
+//     return this.items.push(newItem);
+//   }
+//   removeItem(itemToRemove) {
+//     return this.items.splice(1, 1);
+//   }
+// }
+
+// // Change code above this line
+// const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+// storage.addItem("Droid");
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// storage.removeItem("Prolonger");
+// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+//   module 5 11/20
+
+// class StringBuilder {
+//   constructor(initialValue) {
+//     this.value = initialValue;
+// }
+// getValue(value) {
+//   return this.value;
+// }
+// padEnd(str) {
+// this.value = this.value + str;
+// }
+// padStart(str) {
+// this.value = str + this.value;
+// }
+// padBoth(str) {
+// this.value = str + this.value + str;
+// }
+// }
+
+// // Change code above this line
+// const builder = new StringBuilder(".");
+// console.log(builder.getValue()); // "."
+// builder.padStart("^");
+// console.log(builder.getValue()); // "^."
+// builder.padEnd("^");
+// console.log(builder.getValue()); // "^.^"
+// builder.padBoth("=");
+// console.log(builder.getValue()); // "=^.^="
+
+// module 5 12/20
+
+// class Car {
+//   // Change code below this line
+// #brand;
+//   constructor({ brand, model, price }) {
+//     this.#brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+//   getBrand() {
+//     return this.#brand;
+//   }
+// changeBrand (newBrand) {
+//   this.#brand = newBrand;
+// }
+//   // Change code above this line
+// }
+
+// module 5 13/20
+
+// class Storage {
+//   // Change code below this line
+// #items;
+//   constructor(items) {
+//     this.#items = items;
+//   }
+
+//   getItems() {
+//     return this.#items;
+//   }
+
+//   addItem(newItem) {
+//     this.#items.push(newItem);
+//   }
+
+//   removeItem(itemToRemove) {
+//     this.#items = this.#items.filter(item => item !== itemToRemove);
+//   }
+// }
+
+// // Change code above this line
+// const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+// storage.addItem("Droid");
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// storage.removeItem("Prolonger");
+// console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Droid"]
+  
+// module 5 14/20
+
+// class StringBuilder {
+//   // Change code below this line
+// #value;
+//   constructor(initialValue) {
+//     this.#value = initialValue;
+//   }
+
+//   getValue() {
+//     return this.#value;
+//   }
+
+//   padEnd(str) {
+//     this.#value += str;
+//   }
+
+//   padStart(str) {
+//     this.#value = str + this.#value;
+//   }
+
+//   padBoth(str) {
+//     this.padStart(str);
+//     this.padEnd(str);
+//   }
+// }
+
+// // Change code above this line
+// const builder = new StringBuilder(".");
+// console.log(builder.getValue()); // "."
+// builder.padStart("^");
+// console.log(builder.getValue()); // "^."
+// builder.padEnd("^");
+// console.log(builder.getValue()); // "^.^"
+// builder.padBoth("=");
+// console.log(builder.getValue()); // "=^.^="
+
+// module 5 15/20
+
+// class Car {
+//   // Change code below this line
+//   #brand;
+// #model;
+//   #price;
+//   constructor({ brand, model, price }) {
+//     this.#brand = brand;
+//     this.#model = model;
+//     this.#price = price;
+//   }
+// get brand() {
+//   return this.#brand;
+//  }
+//   set brand(ewBrand) {
+//     this.#brand = newBrand;
+//   }
+//   // getBrand() {
+//   //   return this.#brand;
+//   // }
+//   // setBrand() {
+//   //   return this.#brand;
+//   // }
+
+//   changeBrand(newBrand) {
+//     this.#brand = newBrand;
+//   }
+
+//   get model() {
+//     return this.model;
+//   }
+//   // getModel() {
+//   //   return this.model;
+//   // }
+//   set model(newModel) {
+//     this.model = newModel;
+//   }
+//   // updateModel(newModel) {
+//   //   this.model = newModel;
+//   // }
+//   get price() {
+//     return this.price;
+//   }
+//   // getPrice() {
+//   //   return this.price;
+//   // }
+//   set price(newPrice) {
+//     this.price = newPrice
+//   }
+//   // setPrice(newPrice) {
+//   //   this.price = newPrice;
+//   // }
+//   // Change code above this line
+// }
+
+// module 5 16/20
